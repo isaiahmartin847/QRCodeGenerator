a93166bccb942cd0e97cdb9cc0fd03950da86ab9
isaiah <isaiahmartin847@gmail.com>
2024-04-28 16:17:12 -0600

    added the right styling and did a sliding bar instead of a drop down

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-console.log("the js has loaded")
-
-
+const slider = document.getElementById("myRange");
 const form = document.getElementById("qr-code-form")
 const data = document.getElementById("qr-code-data")
 const size = document.getElementById("dropdown")
@@ -11,6 +9,10 @@ form.addEventListener("submit", (event) => {
     if(!data.value || size.value === "null") {
         alert("please finish filling out the form")
     }
-
-    
 })
+
+slider.oninput = function() {
+    
+    console.log(this.value)
+  }
+
