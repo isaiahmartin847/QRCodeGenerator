f4d4f77808404f20205231c2f3684a2dfb555b24
isaiah <isaiahmartin847@gmail.com>
2024-04-28 18:41:43 -0600

    added the toggling if the form is not filled out a toast will show up

--- js/main.js
+++ js/main.js
@@ -2,6 +2,10 @@ const slider = document.getElementById("myRange");
 const form = document.getElementById("qr-code-form")
 const data = document.getElementById("qr-code-data")
 const size = document.getElementById("dropdown")
+const toggle = document.getElementById("toggle");
+
+
+// toggle.style.display = "block"
 
 
 form.addEventListener("submit", (event) => {
@@ -13,6 +17,6 @@ form.addEventListener("submit", (event) => {
 
 slider.oninput = function() {
     
-    console.log(this.value)
+    
   }
 
